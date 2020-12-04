4f07db91dc4bd3c41d89b9a15b89ca9072b10390
JeroenKortus <75486729+JeroenKortus@users.noreply.github.com>
2020-12-04 15:18:20 +0100

    Add files via upload

---
+++ frame_extraction.py
@@ -0,0 +1,96 @@
+# This script is a modified version of Hayden Faulkner's script.
+# The original script is available at https://gist.github.com/HaydenFaulkner/3aa69130017d6405a8c0580c63bee8e6
+
+import cv2
+import os
+import numpy as np
+import glob
+
+def extract_frames(video_path, frames_dir, overwrite=False, start=-1, end=-1, amount=1):
+    """
+    Extract frames from a video using OpenCVs VideoCapture
+    :param video_path: path of the video
+    :param frames_dir: the directory to save the frames
+    :param overwrite: to overwrite frames that already exist?
+    :param start: start frame
+    :param end: end frame
+    :param amount: frames extracted per video
+    :return: count of images saved
+    """
+
+    video_path = os.path.normpath(video_path)  # make the paths OS (Windows) compatible
+    frames_dir = os.path.normpath(frames_dir)  # make the paths OS (Windows) compatible
+
+    video_dir, video_filename = os.path.split(video_path)  # get the video path and filename from the path
+
+    assert os.path.exists(video_path)  # assert the video file exists
+
+    capture = cv2.VideoCapture(video_path)  # open the video using OpenCV
+
+    if start < 0:  # if start isn't specified lets assume 0
+        start = 0
+    if end < 0:  # if end isn't specified assume the end of the video
+        end = int(capture.get(cv2.CAP_PROP_FRAME_COUNT))
+    
+    every = int(np.ceil(end/amount))
+    capture.set(1, start)  # set the starting frame of the capture
+    frame = start  # keep track of which frame we are up to, starting from start
+    while_safety = 0  # a safety counter to ensure we don't enter an infinite while loop (hopefully we won't need it)
+    saved_count = 0  # a count of how many frames we have saved
+
+    while frame < end:  # lets loop through the frames until the end
+
+        _, image = capture.read()  # read an image from the capture
+
+        if while_safety > 500:  # break the while if our safety maxs out at 500
+            break
+
+        # sometimes OpenCV reads None's during a video, in which case we want to just skip
+        if image is None:  # if we get a bad return flag or the image we read is None, lets not save
+            while_safety += 1  # add 1 to our while safety, since we skip before incrementing our frame variable
+            continue  # skip
+
+        if frame % every == 0:  # if this is a frame we want to write out based on the 'every' argument
+            while_safety = 0  # reset the safety count
+            save_path = os.path.join(frames_dir, video_filename[0:-4] + "_{:04d}.jpg".format(frame))  # create the save path
+            if not os.path.exists(save_path) or overwrite:  # if it doesn't exist or we want to overwrite anyways
+                cv2.imwrite(save_path, image)  # save the extracted image
+                saved_count += 1  # increment our counter by one
+
+        frame += 1  # increment our frame count
+
+    capture.release()  # after the while has finished close the capture
+
+    return saved_count  # and return the count of the images we saved
+
+def video_to_frames(video_path, frames_dir, overwrite=False, amount=1):
+    """
+    Extracts the frames from a video
+    :param video_path: path to the video
+    :param frames_dir: directory to save the frames
+    :param overwrite: overwrite frames if they exist?
+    :param amount: extract this many frames
+    :return: path to the directory where the frames were saved, or None if fails
+    """
+
+    video_path = os.path.normpath(video_path)  # make the paths OS (Windows) compatible
+    frames_dir = os.path.normpath(frames_dir)  # make the paths OS (Windows) compatible
+
+    video_dir, video_filename = os.path.split(video_path)  # get the video path and filename from the path
+
+    # make directory to save frames, its a sub dir in the frames_dir with the video name
+    # os.makedirs(os.path.join(frames_dir, video_filename), exist_ok=True)
+    
+    print("Extracting frames from {}".format(video_filename))
+    
+    extract_frames(video_path, frames_dir, amount=amount)  # let's now extract the frames
+
+    return os.path.join(frames_dir, video_filename)  # when done return the directory containing the frames
+
+output_path = "FaceForensics_dataset/original"
+video_files = glob.iglob(r'C:/path/to/videos/for/frame/extraction/*.mp4')
+
+print(video_files)
+if __name__ == '__main__':
+    for filepath in video_files:
+        video_to_frames(video_path=filepath, frames_dir=output_path, overwrite=False, amount=5)
